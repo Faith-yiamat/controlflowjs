537e63bca9ccd89a6f7a65449588c23fa5725184
Faith-yiamat <oloserianyiamat@gmail.com>
2024-04-17 07:12:19 +0300

    add file

--- controlflow.js
+++ controlflow.js
@@ -1,18 +1,18 @@
-  //question1
+//   question1
 
 function manipulate(array){
 
     const newArray = []
    array.slice(0,4).forEach(item =>{
-        newArray.push(item * item);
-    })
+     newArray.push(item * item)
+    });
 
-    newArray.push(array.length-1 +10);
     newArray.push(array.length-2 +10);
-    return newArray
+    newArray.push(array.length-1 +10);
+    return  newArray
 
 }
-  const array = [1,2,3,4,5,6,7];
+  const array = [1,2,3,4,5,6];
   console.log(manipulate(array));
         
 //question 2
@@ -30,45 +30,34 @@ mynum(arrNum)
 
 //question3
 function contin(fruits){
-  fruits.forEach((frui, index) =>{
+  for(let index = 0; index < fruits.length; index++){
     if(index ===1 ){
-      return;
+         continue;
     }
-    console.log(frui)
-  });
+    console.log(fruits[index])
+  };
 
 }
-let fruits= ['apple','plum','banana','strawberries','kiwi'];
-contin(fruits);
+contin(['apple','plum','banana','strawberries','kiwi']);
 
 //question 4
 function acceptsArray(strArr){
   for(let i= 0; i<strArr.length; i++){
-
-  
-  console.log(strArr[i]);
+   console.log(strArr[i]);
   }
 }
-const strArr = ["Faith","Oloserian","Yiamat","Abdo"];
-acceptsArray(strArr);
+acceptsArray(["Faith","Oloserian","Yiamat","Abdo"]);
 
 
 //question 5
-function reverseStr(Stringrev){
-  let reverse = "";
-  let i = 0;
- // for(let i=Stringrev.length-1;i>=0; i--)//{
-    while(i<Stringrev.length-1 && i>0){
-      reverse+= Stringrev[i];
-
-    
-  }
-  console.log(reverse);
-    return reverse
-  
+function reverseStr(stringrev){
+while(y = stringrev.split("").reverse().join("")){
+  console.log(y)
+  break
 }
-const Stringrev = "myname";
-console.log(reverseStr(Stringrev));
+
+}
+reverseStr("yiamat");
   
 
 
